59a8242690223f374906f83444e2106d579a6a35
lucasb <lkab05@hotmail.com>
2025-09-14 22:49:10 -0300

    feat: dropzone upload created

---
+++ src/components/upload-widget-dropzone.tsx
@@ -0,0 +1,28 @@
+import { useDropzone } from 'react-dropzone';
+
+export function UploadWidgetDropzone() {
+  const { getInputProps, getRootProps, isDragActive } = useDropzone({
+    multiple: true,
+    accept: {
+      'image/jpeg': [],
+      'image/png': [],
+    },
+    onDrop(acceptedFiles) {},
+  });
+
+  return (
+    <div className="px-3 flex flex-col gap-3">
+      <div
+        data-active={isDragActive}
+        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col justify-center items-center gap-1 hover:border-zinc-400 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-300"
+        {...getRootProps()}
+      >
+        <input type="file" {...getInputProps()} />
+        <span className="text-xs">Drop your files hre or</span>
+        <span className="text-xs underline">click to open picker</span>
+      </div>
+
+      <span className="text-xs  text-zinc-400">Only PNG and JPG files are supported.</span>
+    </div>
+  );
+}
