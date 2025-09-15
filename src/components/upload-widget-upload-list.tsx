6a720225306dca1e3d60f54b99e155181de51a0e
lucasb <lkab05@hotmail.com>
2025-09-15 12:49:35 -0300

    feat: upload item list component

---
+++ src/components/upload-widget-upload-list.tsx
@@ -0,0 +1,15 @@
+import { UploadWidgetUploadItem } from './upload-widget-upload-item';
+
+export function UploadWidgetList() {
+  return (
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+      <div className="flex flex-col gap-2">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
+  );
+}
